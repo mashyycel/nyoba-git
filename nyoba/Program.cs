9fac233f96d30e60f0d6cdcb33c503d4795d9c5b
ASUS <ASUS@LAPTOP-IIK6988I>
2021-12-03 08:58:24 +0700

    Add project files.

---
+++ nyoba/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace nyoba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
